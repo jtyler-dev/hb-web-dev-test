a62fe67628f86b8717b85ecda4b532ebb9a8ca7f
jtyler <jtyler.dev@gmail.com>
2017-01-22 22:31:11 -0800

    adding in last of styling of contact cards

--- app/contact-list/contact-list.component.js
+++ app/contact-list/contact-list.component.js
@@ -7,9 +7,12 @@ angular.
     controller: [ '$http',
       function ContactListController($http) {
           var that = this;
+          that.displayContacts = true;
+
           $http.get('https://candidate-test.herokuapp.com/contacts').
           then(function(response) {
             that.contacts = response.data;
+            that.displayContacts = false;
           });
       }
     ]
